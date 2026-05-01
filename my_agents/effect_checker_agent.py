5c5645c93b40f56c41735fa5eba21f03535ab372
TR2335 <YX66vy27@163.com>
2026-05-01 14:40:30 +0800

    Update my_agents/effect_checker_agent.py

--- my_agents/effect_checker_agent.py
+++ my_agents/effect_checker_agent.py
@@ -2,7 +2,7 @@ from langchain_core.runnables import Runnable
 from langchain_core.messages import SystemMessage, HumanMessage
 from pydantic import BaseModel, Field
 from typing import List
-from __004__langgraph_agent.agent_state import AgentState
+from agent_state import AgentState
 from common.llm import my_llm
 import json
 
